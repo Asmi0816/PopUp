5aca90d4640f9157b30d9ad9892a1c5741811bfb
Arick Smith <kidwriter2020@gmail.com>
2016-09-27 12:23:12 -0600

    Added y output method to the popup class

--- src/popup/view/PopUpViewer.java
+++ src/popup/view/PopUpViewer.java
@@ -1,6 +1,15 @@
 package popup.view;
 
+import javax.swing.JOptionPane;
+
+
 public class PopUpViewer 
 {
-
+	
+ public void displayMessage(String message)
+ {
+	 JOptionPane.showMessageDialog(null, message);
+	 //will replace the null when a owner is made
+ }
+ 
 }
