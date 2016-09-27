171521f82f0a7016680d8c1ef60fc1568820b12f
Arick Smith <kidwriter2020@gmail.com>
2016-09-27 12:41:34 -0600

    Made another method for popup collection.

--- src/popup/view/PopUpViewer.java
+++ src/popup/view/PopUpViewer.java
@@ -5,9 +5,14 @@ import javax.swing.JOptionPane;
 
 public class PopUpViewer 
 {
+	/**
+	 * Makes a popup message to show a user a String.
+	 * @param message The String to show the user.
+	 */
 	
  public void displayMessage(String message)
  {
+	 
 	 JOptionPane.showMessageDialog(null, message);
 	 //will replace the null when a owner is made
  }
